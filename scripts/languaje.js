2f4138adc326777a4f1f7249ecd9720d8b3d70c3
Josep Sànchez Arbona <47741862+daskza19@users.noreply.github.com>
2025-03-22 16:44:56 +0100

    Team Page implemented

--- scripts/languaje.js
+++ scripts/languaje.js
@@ -3,6 +3,10 @@ document.addEventListener("DOMContentLoaded", () => {
     const dropdown = document.getElementById("dropdown");
     const options = document.querySelectorAll(".option");
     const watchTrailerElement = document.getElementById("watch_trailer");
+    const screenshotTitleElement = document.getElementById("screenshot_title");
+    const charactersTitleElement = document.getElementById("characters_title");
+    const teamTitleElement = document.getElementById("team_title");
+    const project_titleTitleElement = document.getElementById("project_title");
 
     // Diccionario de idiomas
     const watchTrailerTranslations = {
@@ -10,7 +14,26 @@ document.addEventListener("DOMContentLoaded", () => {
         en: "WATCH TRAILER",
         ca: "VEURE TRÁILER"
     };
-
+    const projectTitleTranslations = {
+        es: "PROYECTO",
+        en: "PROJECT",
+        ca: "PROJECTE"
+    };
+    const charactersTitleTranslations = {
+        es: "PERSONAJES",
+        en: "CHARACTERS",
+        ca: "PERSONATGES"
+    };
+    const teamTitleTranslations = {
+        es: "EQUIPO",
+        en: "TEAM",
+        ca: "EQUIP"
+    };
+    const screenshotsTitleTranslations = {
+        es: "CAPTURAS",
+        en: "SCREENSHOTS",
+        ca: "CAPTURES"
+    };
 
     // Mostrar / ocultar menú
     mainBtn.addEventListener("click", () => {
@@ -29,6 +52,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
             // Cambiar el texto al idioma seleccionado
             watchTrailerElement.textContent = watchTrailerTranslations[selectedLang];
+            project_titleTitleElement.textContent = projectTitleTranslations[selectedLang];
+            charactersTitleElement.textContent = charactersTitleTranslations[selectedLang];
+            teamTitleElement.textContent = teamTitleTranslations[selectedLang];
+            screenshotTitleElement.textContent = screenshotsTitleTranslations[selectedLang];
 
             // Ocultar el menú con animación
             dropdown.classList.remove("visible");
