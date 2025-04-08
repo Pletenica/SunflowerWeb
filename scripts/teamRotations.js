e0cd654b767777861e542eb1b261bfca5a186a7a
Josep Sànchez Arbona <josep.sanchez.arbona@gmail.com>
2025-04-08 20:27:57 +0200

    new commit

--- scripts/teamRotations.js
+++ scripts/teamRotations.js
@@ -4,15 +4,23 @@ function getRandomRotation(min, max) {
 
 // Asignar la rotación inicial aleatoria
 window.onload = function() {
-    const minRotation = -1.5;  // Ángulo mínimo
-    const maxRotation = 1.5;   // Ángulo máximo
-    const rotation = getRandomRotation(minRotation, maxRotation);
+    const minRotation = -1.5;
+    const maxRotation = 3;
     document.getElementById("josep_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
-    
     document.getElementById("roger_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
     document.getElementById("arnau_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
     document.getElementById("javier_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
     document.getElementById("silvino_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
     document.getElementById("adria_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
     document.getElementById("lucas_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+
+    document.getElementById("drandkiley_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("mat_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("bomber_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("krabverted_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("professor_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("boss_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("dartcolepsy_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("reflection_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
+    document.getElementById("minion_card").style.transform = `rotate(${getRandomRotation(minRotation, maxRotation)}deg)`;
 };
